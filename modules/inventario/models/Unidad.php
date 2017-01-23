a9c8778b7dafde6ac47371cbffeba0c4301f6480
JeancarloFontalvo <jeancarlofontalvo@gmail.com>
2017-01-22 22:23:17 -0500

    Añadido helper para obtener la unidad por Item
    - getItemUnity: Retorna la unidad (texto) en funcion del item, si es
    consumible o no

--- modules/inventario/models/Unidad.php
+++ modules/inventario/models/Unidad.php
@@ -3,7 +3,9 @@
 namespace app\modules\inventario\models;
 
 use Yii;
-
+use app\modules\inventario\models\core\Items;
+use app\modules\inventario\models\core\TipoItem;
+use app\modules\inventario\models\Reactivo;
 /**
  * This is the model class for table "TBL_UNIDADES".
  *
@@ -42,5 +44,21 @@ class Unidad extends \yii\db\ActiveRecord
             'UNID_NOMBRE' => 'Unid  Nombre',
             'UNID_DESCRIPCION' => 'Unid  Descripcion',
         ];
+    }
+
+    public static function getItemUnity($id)
+    {
+        $isReactive = Items::findOne($id)->TIIT_ID == TipoItem::Reactivo;
+        
+        if($isReactive) {
+            $reactive = Reactivo::getByItemId($id);
+            
+            if($reactive !== null) {
+                $unit = $reactive->unidad;
+                return isset($unit) ? $unit->UNID_NOMBRE : "";
+            }
+        }
+        
+        return "UND";
     }
 }
