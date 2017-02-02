738dc1bfa3f0264c5e031e7660619d9597028b3e
jaiderjksj <jaidersierra2009@gmail.com>
2017-02-02 18:39:05 -0500

    Carpeta de documentacion
    CArpeta donde se iran trabajando los archivos de la documentacion, el
    archivo .mdj es del caso de uso en star uml

--- views/site/index.php
+++ views/site/index.php
@@ -7,43 +7,48 @@ $this->title = 'Inventario SILAB';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Felicitaciones con Github Desktop!!</h1>
+        <h1>Sistema integral de laboratorios</h1>
 
-        <p class="lead">Este aplicativo sera la genial :D</p>
-
-        <p><a class="btn btn-lg btn-primary" href="http://www.yiiframework.com">Comenzar yii</a></p>
-    </div>
-
-    <div class="body-content">
+       
+        <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4">
-                <h2>Inventarios</h2>
+                <h2>Definicion</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p align="justify">El Sistema Integral de Laboratorios (SILAB) es una unidad funcional que
+                    integra o centraliza la actividad misional de la universidad de La Guajira
+                    relacionada con las actividades de experimentación e investigación según las
+                    áreas de las ciencias; facilitando y permitiendo la integración institucional de los
+                    procesos de generación, transmisión y difusión del conocimiento.
+                    </p>
 
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Productos</h2>
+                <h2>Vision del SILAB</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p align="justify">Para el año 2014, El Sistema Integral de Laboratorios (SILAB) de la
+                    Universidad de La Guajira será reconocido como una unidad funcional que
+                    presta los servicios técnicos especializados, investigativos y de asesoría; por
+                    medio del correcto y confiable desarrollo de procesos y procedimientos de
+                    laboratorio, facilitando su uso a lo largo y ancho de todas las áreas de las
+                    unidades académicas de la institución que requieran el desarrollo de clases
+                    prácticas y otros trabajos relacionados con las funciones de docencia.
+                    Investigación y proyección social</p>
 
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>proveedores</h2>
+                <h2>Mision del SILAB</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p align="justify">El Sistema Integral de laboratorios (SILAB) tiene como propósito articular las
+                    actividades académicas e investigativas de la Universidad de La Guajira con
+                    las actividades propias de la comunidad en general, a través de la prestación
+                    de servicios técnicos especializados, investigativos y de asesoría; sustentadas
+                    en las competencias laborales e intelectuales de las personas que lo integran y
+                    el compromiso con el desarrollo de la ciencia y la tecnología de la región y del
+                    país.</p>
 
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
             </div>
